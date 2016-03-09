ba6dea381221ca518e2b08ff7f285c567d75e62d
Klaas Sangers <klaas@sangers.nu>
2016-03-09 12:22:52 +0100

    added arch to google chrome

--- install-env-dev.sh
+++ install-env-dev.sh
@@ -76,7 +76,7 @@ echo Installing PPA\'s
 if [ ! -f /etc/apt/sources.list.d/google-chrome.list ]
 then
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -  > $VERBOSE
-    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 fi
 
 if [ "$EXTRA_BTSEC" -eq "1" ]
